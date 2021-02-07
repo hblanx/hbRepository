22818bb92a2634ce24d4631dd4d27625bc4330e0
hblanx <60420735+hblanx@users.noreply.github.com>
2021-02-07 13:11:25 +0800

    添加了低性能的增加节点功能

--- KD树/hbKDtree.py
+++ KD树/hbKDtree.py
@@ -12,7 +12,8 @@ class Node:
         self.y = y
         self.deep = deep
         self.childNum = childNum
-
+        self.left = False
+        self.right = False
 
 '''
 Tree类为kd树
@@ -22,6 +23,7 @@ createNode()方法，生成节点
 knn()方法，进行k的邻近搜索，此方法为接口
 search()方法，根据条件遍历子树
 compare()方法，比较距离
+add()方法，增加节点，注意，此方法会导致熵减速度下降！
 '''
 class Tree:
     def __init__(self):
@@ -53,6 +55,8 @@ class Tree:
             node = Node(x, y, deep, 2)
             node.left = self.createNode(leftList, deep + 1)
             node.right = self.createNode(rightList, deep + 1)
+            
+
 
 
         elif len(data) == 2:  # 只有一个子节点
@@ -66,17 +70,49 @@ class Tree:
 
             node = Node(x, y, deep, 1)
             node.left = self.createNode(smallData, deep + 1)
-            node.right = False
+
 
         else:  # 此节点就是叶子节点
             x = data[0][0]
             y = data[0][1]
-
+            
             node = Node(x, y, deep, 0)
-            node.left = False
-            node.right = False
 
         return node
+    
+    def rebuild(self,node,aimX,aimY):
+        leaf = node.left
+        data=[[leaf.x,leaf.y],[node.x,node.y],[aimX,aimY]]
+        return self.createNode(data, node.deep)
+        
+    
+    def addNode(self,node,aimX,aimY):
+        if (node.childNum == 2):  # 有2个子节点
+            if (node.deep % 2 == 1):  # 奇数层
+                if (aimX <= node.x):  # 小于等于支节点
+                    node.left = self.addNode(node.left, aimX, aimY)
+                else:
+                    node.right = self.addNode(node.right, aimX, aimY)
+            else:  # 偶数层
+                if (aimY <= node.y):  # 小于等于支节点
+                    node.left = self.addNode(node.left, aimX, aimY)
+                else:
+                    node.right = self.addNode(node.right, aimX, aimY)
+
+            return node
+        elif (node.childNum == 1):  # 只有1个子节点
+            node = self.rebuild(node,aimX,aimY)
+            node.childNum = 2
+            return node
+        
+        else:# 叶子节点
+            newNode = Node(aimX, aimY, node.deep+1, 0)
+            node.left = newNode
+            node.childNum = 1
+            return node
+
+    def add(self,aimX,aimY):
+        self.head = self.addNode(self.head, aimX, aimY)
 
     def knn(self, aimX, aimY, size):
         S = [[999, 0, 0] for i in range(3)]
@@ -129,8 +165,11 @@ class Tree:
 if __name__ == "__main__":
     items = [[6, 5], [1, -3], [-6, -5], [-4, -10], [-2, -1], [-5, 12], [2, 13], [17, -12], [8, -22], [15, -13],
              [10, -6], [7, 15], [14, 1]]
+    testItmes = [[6, 5], [1, -3], [-6, -5], [-4, -10],  [-5, 12], [2, 13], [17, -12], [8, -22], [15, -13],
+             [10, -6], [7, 15], [14, 1]]
     tree = Tree()
-    tree.data = items
+    tree.data = testItmes
     tree.create()
+    tree.add(-2, -1)
     ans = tree.knn(-1, -5, 3)
 
