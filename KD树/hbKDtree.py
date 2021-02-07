0ed275d8a15251db3f1322ed2bdca63f4a26c254
hblanx <60420735+hblanx@users.noreply.github.com>
2021-02-07 13:24:22 +0800

    bug修复

--- KD树/hbKDtree.py
+++ KD树/hbKDtree.py
@@ -15,6 +15,7 @@ class Node:
         self.left = False
         self.right = False
 
+
 '''
 Tree类为kd树
 sortX(),sortY()方法分别对包含xy轴数据的列表进行x排序和y排序
@@ -23,7 +24,9 @@ createNode()方法，生成节点
 knn()方法，进行k的邻近搜索，此方法为接口
 search()方法，根据条件遍历子树
 compare()方法，比较距离
-add()方法，增加节点，注意，此方法会导致熵减速度下降！
+add()方法，增加节点，注意，此方法会导致熵减速度下降！此方法为接口
+rebuild()方法，在增加节点时重建含有一个子节点的分支节点
+addNode()方法，递归增加节点
 '''
 class Tree:
     def __init__(self):
@@ -36,7 +39,10 @@ class Tree:
 
     def sortY(self, inputList):
         return inputList[1]
-
+    '''
+    create方法，使用Tree类的data成员变量构造树
+    生成成员变量head 无返回值
+    '''
     def create(self):
         self.head = self.createNode(self.data, 1)
 
@@ -55,7 +61,7 @@ class Tree:
             node = Node(x, y, deep, 2)
             node.left = self.createNode(leftList, deep + 1)
             node.right = self.createNode(rightList, deep + 1)
-            
+
 
 
 
@@ -75,18 +81,17 @@ class Tree:
         else:  # 此节点就是叶子节点
             x = data[0][0]
             y = data[0][1]
-            
+
             node = Node(x, y, deep, 0)
 
         return node
-    
-    def rebuild(self,node,aimX,aimY):
+
+    def rebuild(self, node, aimX, aimY):
         leaf = node.left
-        data=[[leaf.x,leaf.y],[node.x,node.y],[aimX,aimY]]
+        data = [[leaf.x, leaf.y], [node.x, node.y], [aimX, aimY]]
         return self.createNode(data, node.deep)
-        
-    
-    def addNode(self,node,aimX,aimY):
+
+    def addNode(self, node, aimX, aimY):
         if (node.childNum == 2):  # 有2个子节点
             if (node.deep % 2 == 1):  # 奇数层
                 if (aimX <= node.x):  # 小于等于支节点
@@ -101,21 +106,28 @@ class Tree:
 
             return node
         elif (node.childNum == 1):  # 只有1个子节点
-            node = self.rebuild(node,aimX,aimY)
+            node = self.rebuild(node, aimX, aimY)
             node.childNum = 2
             return node
-        
-        else:# 叶子节点
-            newNode = Node(aimX, aimY, node.deep+1, 0)
+
+        else:  # 叶子节点
+            newNode = Node(aimX, aimY, node.deep + 1, 0)
             node.left = newNode
             node.childNum = 1
             return node
-
-    def add(self,aimX,aimY):
+    '''
+    add方法，输入新增物体的x值、y值
+    无返回值
+    '''
+    def add(self, aimX, aimY):
         self.head = self.addNode(self.head, aimX, aimY)
-
+        self.data.append([aimX,aimY])
+    '''
+    knn方法，输入目标物体的x值、y值，搜索的最近目标数量
+    返回目标信息元组，信息包含距离、x值、y值
+    '''
     def knn(self, aimX, aimY, size):
-        S = [[999, 0, 0] for i in range(3)]
+        S = [[999, 0, 0] for i in range(size)]
         assert self.head, "未生成树"
         self.search(self.head, aimX, aimY, S)
         return tuple(S)
@@ -165,11 +177,10 @@ class Tree:
 if __name__ == "__main__":
     items = [[6, 5], [1, -3], [-6, -5], [-4, -10], [-2, -1], [-5, 12], [2, 13], [17, -12], [8, -22], [15, -13],
              [10, -6], [7, 15], [14, 1]]
-    testItmes = [[6, 5], [1, -3], [-6, -5], [-4, -10],  [-5, 12], [2, 13], [17, -12], [8, -22], [15, -13],
-             [10, -6], [7, 15], [14, 1]]
+    testItmes = [[6, 5], [1, -3], [-6, -5], [-4, -10], [-5, 12], [2, 13], [17, -12], [8, -22], [15, -13],
+                 [10, -6], [7, 15], [14, 1]]
     tree = Tree()
     tree.data = testItmes
     tree.create()
     tree.add(-2, -1)
     ans = tree.knn(-1, -5, 3)
-
